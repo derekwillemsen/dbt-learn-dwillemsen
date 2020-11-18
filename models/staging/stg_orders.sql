77a573f9f17575b9f830a0b9521549aba81aa77d
Derek Willemsen <derek.willemsen@getbux.com>
2020-11-18 14:58:58 +0000

    Added lifetime value to customer model

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,22 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+with 
+    orders as (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
 
-from {{ source('jaffle_shop', 'orders') }}
+    from {{ source('jaffle_shop', 'orders') }}
+)
+
+, final as (
+    select 
+          orders.order_id
+        , orders.customer_id
+        , orders.order_date
+        , orders.status
+    from    
+        orders
+)
+
+select * from final
